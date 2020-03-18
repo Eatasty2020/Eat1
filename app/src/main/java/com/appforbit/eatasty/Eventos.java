8028ac0f8fd748ec1ed2c6531300ff4d6a73149c
Gerardo Lorenzo <61482918+GerardoLorenzo@users.noreply.github.com>
2020-03-18 00:32:30 -0300

    Initial commit

---
+++ app/src/main/java/com/appforbit/eatasty/Eventos.java
@@ -0,0 +1,13 @@
+package com.appforbit.eatasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Eventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos);
+    }
+}
---
+++ app/src/main/java/com/appforbit/eatasty/MainActivity.java
@@ -0,0 +1,27 @@
+package com.appforbit.eatasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Lanzador de Login
+    public void Login(View view){
+        Intent login = new Intent(this, Login.class);
+        startActivity(login);
+    }
+
+    //Lanzador de Eventos
+    public void Evento(View view){
+        Intent evento = new Intent(this, Eventos.class);
+        startActivity(evento);
+    }
+}
